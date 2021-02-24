15a4afa9a57689476e298ca16b9eb5fbbb5ed86c
yinfoyuan <3485201956@qq.com>
2021-02-24 17:38:31 +0800

    feat(config-provider): add global configuration

---
+++ packages/config-provider/src/index.vue
@@ -0,0 +1,28 @@
+<script lang="ts">
+import { defineComponent, provide, toRef } from 'vue'
+import { textOptionsKey } from '@oy/utils/symbol-key'
+
+import type { TextOptions } from '@oy/utils/common.type'
+import type { PropType } from 'vue'
+
+export default defineComponent({
+  name: 'OyConfigProvider',
+  props: {
+    textOptions: {
+      type: Object as PropType<TextOptions>
+    }
+  },
+  setup (props) {
+    provide(textOptionsKey, toRef(props, 'textOptions'))
+
+    return {}
+  },
+  render () {
+    return this.$slots.default  ? this.$slots.default() : null
+  }
+})
+</script>
+
+<style scoped>
+
+</style>
